d0617b120f3c427329e039c00988ae7a05f49d80
Abirvalgo <113058649+Abirvalgo@users.noreply.github.com>
2023-04-05 19:27:22 +0300

    add basic {Router}, colors.scss, font, mixins

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+const Home = () => {
+  return <div className={styles.container}>Font check</div>;
+};
+
+export default Home;
