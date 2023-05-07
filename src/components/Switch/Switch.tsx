b05602bb303247941ca873b1955cdeed235760eb
Abirvalgo <113058649+Abirvalgo@users.noreply.github.com>
2023-05-07 19:15:52 +0300

    small change switch component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,32 +1,23 @@
-import React, { ChangeEvent, FC, useState } from "react";
+import React, { FC, MouseEventHandler } from "react";
 import styles from "./Switch.module.scss";
 import classNames from "classnames";
-import { useThemeContext } from "../../context/Theme/Context";
 
 type SwitchProps = {
   checked?: boolean | undefined;
   disabled?: boolean | undefined;
-  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onClick?: MouseEventHandler<HTMLInputElement>;
 };
 
-const Switch: FC<SwitchProps> = ({ checked, disabled }) => {
-  const { theme, onChangeTheme } = useThemeContext();
-  const onClick = (value: boolean) => () => onChangeTheme(value);
-  // const [isChecked, setIsChecked] = useState(true);
-  // const onChange = () => {
-  //   setIsChecked(!isChecked);
-  // };
-
+const Switch: FC<SwitchProps> = ({ checked, disabled, onClick }) => {
   return (
     <div className={styles.container}>
       <label className={styles.switch}>
         <input
           type="checkbox"
-          checked={theme}
-          onClick={onClick(!theme)}
+          checked={checked}
+          onClick={onClick}
           readOnly
-          // onChange={() => setIsChecked(!isChecked)}
-          // onChange={onChange}
+          disabled={disabled}
         />
         <span className={classNames(styles.slider, styles.round, {})}></span>
       </label>
